8549f3a0af97e5d0be36b5ed6eb03d96df47c2e4
intx4 <francesco.intoci@protonmail.com>
2023-04-06 17:29:44 +0300

    fixed open5gs setup script

--- open5gs_setup.sh
+++ open5gs_setup.sh
@@ -10,60 +10,28 @@ sudo apt install -y mongodb-org
 sudo systemctl start mongod
 sudo systemctl enable mongod
 
+
+
 # TUN
-SYSTEM=`uname`;
-if [ "$SYSTEM" = "Linux" ]; then
-    if ! grep "ogstun" /proc/net/dev > /dev/null; then
-        ip tuntap add name ogstun mode tun
-    fi
-    if test "x`sysctl -n net.ipv6.conf.ogstun.disable_ipv6`" = x1; then
-        echo "net.ipv6.conf.ogstun.disable_ipv6=0" > /etc/sysctl.d/30-open5gs.conf
-        sysctl -p /etc/sysctl.d/30-open5gs.conf
-    fi
-    ip addr del 10.45.0.1/16 dev ogstun 2> /dev/null
-    ip addr add 10.45.0.1/16 dev ogstun
-    ip addr del 2001:db8:cafe::1/48 dev ogstun 2> /dev/null
-    ip addr add 2001:db8:cafe::1/48 dev ogstun
-    ip link set ogstun up
-else
-    sysctl -w net.inet.ip.forwarding=1
-    sysctl -w net.inet6.ip6.forwarding=1
-    ifconfig lo0 alias 127.0.0.2 netmask 255.255.255.255
-    ifconfig lo0 alias 127.0.0.3 netmask 255.255.255.255
-    ifconfig lo0 alias 127.0.0.4 netmask 255.255.255.255
-    ifconfig lo0 alias 127.0.0.5 netmask 255.255.255.255
-    ifconfig lo0 alias 127.0.0.6 netmask 255.255.255.255
-    ifconfig lo0 alias 127.0.0.7 netmask 255.255.255.255
-    ifconfig lo0 alias 127.0.0.8 netmask 255.255.255.255
-    ifconfig lo0 alias 127.0.0.9 netmask 255.255.255.255
-    ifconfig lo0 alias 127.0.0.10 netmask 255.255.255.255
-    ifconfig lo0 alias 127.0.0.11 netmask 255.255.255.255
-    ifconfig lo0 alias 127.0.0.12 netmask 255.255.255.255
-    ifconfig lo0 alias 127.0.0.13 netmask 255.255.255.255
-    ifconfig lo0 alias 127.0.0.14 netmask 255.255.255.255
-    ifconfig lo0 alias 127.0.0.15 netmask 255.255.255.255
-    ifconfig lo0 alias 127.0.0.16 netmask 255.255.255.255
-    ifconfig lo0 alias 127.0.0.17 netmask 255.255.255.255
-    ifconfig lo0 alias 127.0.0.18 netmask 255.255.255.255
-    ifconfig lo0 alias 127.0.0.19 netmask 255.255.255.255
-    ifconfig lo0 alias 127.0.0.20 netmask 255.255.255.255
-    ifconfig lo0 alias 127.0.1.10 netmask 255.255.255.255
-    ifconfig lo0 alias 127.0.1.11 netmask 255.255.255.255
-    ifconfig lo0 alias 127.0.1.12 netmask 255.255.255.255
-    if [ "$SYSTEM" = "Darwin" ]; then
-        if ! test -f /etc/pf.anchors/org.open5gs; then
-            sudo sh -c "echo 'nat on {en0} from 10.45.0.0/16 to any -> {en0}' > /etc/pf.anchors/org.open5gs"
-            sudo sh -c "echo 'nat on {en0} from 2001:db8:cafe::1/48 to any -> {en0}' > /etc/pf.anchors/org.open5gs"
-        fi
-        pfctl -e -f /etc/pf.anchors/org.open5gs
-    fi
-fi
+
 
 cd ~
 git clone https://github.com/open5gs/open5gs
 cd open5gs
+
+sudo apt install curl
+curl -fsSL https://deb.nodesource.com/setup_14.x | sudo -E bash -
+sudo apt install nodejs
+cd webui/
+npm ci --no-optional
+cd ..
 sudo apt install -y python3-pip python3-setuptools python3-wheel ninja-build build-essential flex bison git cmake libsctp-dev libgnutls28-dev libgcrypt-dev libssl-dev libidn11-dev libmongoc-dev libbson-dev libyaml-dev libnghttp2-dev libmicrohttpd-dev libcurl4-gnutls-dev libnghttp2-dev libtins-dev libtalloc-dev meson
 meson build --prefix=`pwd`/install
 ninja -C build
 cd build
 ninja install
+
+sudo ip tuntap add name ogstun mode tun
+sudo ip addr add 10.45.0.1/16 dev ogstun
+sudo ip addr add 2001:230:cafe::1/48 dev ogstun
+sudo ip link set ogstun up
