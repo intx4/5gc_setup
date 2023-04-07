302728423d8a372f0674bb80b06aba95cf9e6608
intx4 <francesco.intoci@protonmail.com>
2023-04-07 18:08:28 +0300

    updated mongodb version of 5g install script

--- open5gs_setup.sh
+++ open5gs_setup.sh
@@ -4,13 +4,13 @@
 sudo apt update -y && sudo apt upgrade -y
 sudo apt install wget gnupg
 wget -qO - https://www.mongodb.org/static/pgp/server-6.0.asc | sudo apt-key add -
-echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu focal/mongodb-org/6.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-6.0.list
+echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu focal/mongodb-org/5.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-5.0.list
 sudo apt update -y
 sudo apt install -y mongodb-org
 sudo systemctl start mongod
 sudo systemctl enable mongod
 
-# INSTALL
+# INSTALL v2.6.1
 cd ~
 git clone --branch v2.6.1 https://github.com/open5gs/open5gs
 cd open5gs
@@ -18,16 +18,16 @@ cd open5gs
 sudo apt install -y python3-pip python3-setuptools python3-wheel ninja-build build-essential flex bison git cmake libsctp-dev libgnutls28-dev libgcrypt-dev libssl-dev libidn11-dev libmongoc-dev libbson-dev libyaml-dev libnghttp2-dev libmicrohttpd-dev libcurl4-gnutls-dev libnghttp2-dev libtins-dev libtalloc-dev meson
 meson build --prefix=`pwd`/install
 ninja -C build
-cd build
+cd ~/open5gs/build
 ninja install
 
 # WEBUI
 sudo apt install curl
-curl -fsSL https://deb.nodesource.com/setup_14.x | sudo -E bash -
+curl -fsSL https://deb.nodesource.com/setup_19.x | sudo -E bash -
 sudo apt install nodejs
-cd webui/
+cd ~/open5gs/webui
 npm ci --no-optional
-cd ..
+cd open5gs
 
 # TUN
 sudo ip tuntap add name ogstun mode tun
