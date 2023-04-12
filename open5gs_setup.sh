b26443fd94da30320e86043dac15ecea31bc3cd9
intx4 <francesco.intoci@protonmail.com>
2023-04-12 13:41:40 +0300

    v2.6.2 open5gs

--- open5gs_setup.sh
+++ open5gs_setup.sh
@@ -2,6 +2,9 @@
 
 # MONGO
 sudo apt update -y && sudo apt upgrade -y
+echo "deb http://security.ubuntu.com/ubuntu focal-security main" | sudo tee /etc/apt/sources.list.d/focal-security.list
+sudo apt-get update
+sudo apt-get install libssl1.1
 sudo apt install wget gnupg
 wget -qO - https://www.mongodb.org/static/pgp/server-5.0.asc | sudo apt-key add -
 echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu focal/mongodb-org/5.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-5.0.list
@@ -9,10 +12,11 @@ sudo apt update -y
 sudo apt install -y mongodb-org
 sudo systemctl start mongod
 sudo systemctl enable mongod
+sudo rm /etc/apt/sources.list.d/focal-security.list
 
 # INSTALL v2.6.1
 cd ~
-git clone --branch v2.6.1 https://github.com/open5gs/open5gs
+git clone --branch v2.6.2 https://github.com/open5gs/open5gs
 cd open5gs
 
 sudo apt install -y python3-pip python3-setuptools python3-wheel ninja-build build-essential flex bison git cmake libsctp-dev libgnutls28-dev libgcrypt-dev libssl-dev libidn11-dev libmongoc-dev libbson-dev libyaml-dev libnghttp2-dev libmicrohttpd-dev libcurl4-gnutls-dev libnghttp2-dev libtins-dev libtalloc-dev meson
@@ -28,8 +32,6 @@ sudo apt-get update
 sudo apt install -y nodejs
 cd ~/open5gs/webui
 npm ci
-echo "Modify the package.json file:" 
-echo "$ sed -i '' 's/"scripts": {/"scripts": {\n\t"dev": "webpack-dev-server --host=YOUR_IP_ADDRESS --port=YOUR_PORT",/' package.json"
 
 # TUN
 sudo ip tuntap add name ogstun mode tun && \
@@ -49,11 +51,3 @@ sudo ip6tables -t nat -A POSTROUTING -s 2001:db8:cafe::/48 ! -o ogstun -j MASQUE
 sudo ufw disable
 ### Ensure that the packets in the `INPUT` chain to the `ogstun` interface are accepted
 sudo iptables -I INPUT -i ogstun -j ACCEPT
-
-### Prevent UE's from connecting to the host on which UPF is running
-sudo iptables -I INPUT -s 10.45.0.0/16 -j DROP
-sudo ip6tables -I INPUT -s 2001:db8:cafe::/48 -j DROP
-
-### If your core network runs over multiple hosts, you probably want to block
-### UE originating traffic from accessing other network functions.
-sudo iptables -I FORWARD -s 10.45.0.0/16 -d 127.0.0.0/24 -j DROP
